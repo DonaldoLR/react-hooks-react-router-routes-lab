8e82ca9126004f01c3778eb3a9e5ca8494fcae8c
github username <github email address>
2021-05-29 12:47:02 -0400

    practice working with react router dom

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor, idx) => {
+        return (
+          <div key={idx}>
+            <h1>{actor.name}</h1>
+            <p>Movies:</p>
+            <ul>
+              {actor.movies.map((movie, idx) => {
+                return <li key={idx}>{movie}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie, idx) => {
+        return (
+          <div key={idx}>
+            <h1>Name: {movie.title}</h1>
+            <p>Time: {movie.time}</p>
+            <p>Genres:</p>
+            <ul>
+              {movie.genres.map((genre, idx) => {
+                return <li key={idx}>{genre}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Movies;
